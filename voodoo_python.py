1828ce47a20b304c35fdbf2f558d3dc81a498de5
Virantha Ekanayake <virantha@gmail.com>
2014-02-24 11:02:44 -0500

    more additions

--- voodoo_python.py
+++ voodoo_python.py
@@ -1,6 +1,8 @@
 from os.path import join, dirname, basename
 from voodoo import render_skeleton, prompt
 from os import system
+import os
+import yaml
 
 
 default_context = {
@@ -13,10 +15,22 @@ default_context = {
 
 SKELETON_PATH = join(dirname(__file__), "templates", "python_pkg")
 
+def get_merged_dict(x,y):
+    z = x.copy()
+    z.update(y)
+    return z
+
 def new_project(path, options):
     data = default_context.copy()
-    data['project_name'] = prompt("Project name", basename(path))
-    data['project_title'] = prompt("Project title", data['project_name'])
+
+    yaml_filename = options.pop('config')
+    print("Loading configuration from %s" % yaml_filename)
+    with open(yaml_filename) as f:
+        config_dict = yaml.load(f)
+
+    data = get_merged_dict(data, config_dict)    
+    #data['project_name'] = prompt("Project name", basename(path))
+    #data['project_title'] = prompt("Project title", data['project_name'])
     render_skeleton(SKELETON_PATH, path, data=data, **options)
     return data
 
@@ -26,6 +40,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Create a new project')
     parser.add_argument('path', help='The name or fullpath of the new project')
+    parser.add_argument('config', help='The configuration yaml for the project')
+
     parser.add_argument('-p', '--pretend', action='store_true',
                         help='Run but do not make any changes')
     parser.add_argument('-f', '--force', action='store_true',
@@ -37,8 +53,10 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     da = vars(args)
-    full_context = new_project(da.pop('path'), da)
+    proj_path = da.pop('path')
+    full_context = new_project(proj_path, da)
 
     # Now, make the first_setup zsh script executable
-    system('chmod a+x %(project_name)s/first_setup.zsh' % full_context)
+    full_context['path'] = proj_path
+    system('chmod a+x %(path)s/first_setup.zsh' % full_context)
 
